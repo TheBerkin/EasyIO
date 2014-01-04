650c9916eda41213610cf38be9a79082e68a9e70
TheBerkin <grenzatuna@gmail.com>
2014-01-04 01:49:50 -0600

    Added some documentation and extension methods

--- EasyIO/Bitfield8.cs
+++ EasyIO/Bitfield8.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace EasyIO
 {    
     /// <summary>
-    /// Represents an 8-bit flag set.
+    /// Represents an 8-bit bitfield.
     /// </summary>
     public class Bitfield8
     {
@@ -36,11 +36,17 @@ namespace EasyIO
             set { bits[i] = value; }
         }
 
+        /// <summary>
+        /// Unsets all the flags in the bitfield.
+        /// </summary>
         public void UnsetAll()
         {
             Array.Clear(bits, 0, N);
         }
 
+        /// <summary>
+        /// Sets all the flags in the bitfield.
+        /// </summary>
         public void SetAll()
         {
             for(int i = 0; i < N; i++)
@@ -49,6 +55,10 @@ namespace EasyIO
             }
         }
 
+        /// <summary>
+        /// Returns the number of set flags.
+        /// </summary>
+        /// <returns>The number of set flags.</returns>
         public int GetSetCount()
         {
             int c = 0;
@@ -62,6 +72,10 @@ namespace EasyIO
             return c;
         }
 
+        /// <summary>
+        /// Returns the number of unset flags.
+        /// </summary>
+        /// <returns>The number of unset flags.</returns>
         public int GetUnsetCount()
         {
             int c = 0;
@@ -75,6 +89,9 @@ namespace EasyIO
             return c;
         }
 
+        /// <summary>
+        /// Inverts the flags in the bitfield.
+        /// </summary>
         public void Invert()
         {
             for(int i = 0; i < N; i++)
@@ -83,6 +100,11 @@ namespace EasyIO
             }
         }
 
+        /// <summary>
+        /// Retrieves a bitfield from the specified stream.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <returns>The read bitfield.</returns>
         public static Bitfield8 FromStream(Stream stream)
         {
             int b = stream.ReadByte();
@@ -100,6 +122,19 @@ namespace EasyIO
             return bf;
         }
 
+        /// <summary>
+        /// Writes the bitfield to the specified stream.
+        /// </summary>
+        /// <param name="stream">The stream to write to.</param>
+        public void WriteToStream(Stream stream)
+        {
+            stream.WriteByte(this.GetByte());
+        }
+
+        /// <summary>
+        /// Returns the bitfield as a single unsigned byte.
+        /// </summary>
+        /// <returns></returns>
         public byte GetByte()
         {
             int b = 0;
--- EasyIO/Extensions.cs
+++ EasyIO/Extensions.cs
@@ -2,53 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace EasyIO
 {
-    static class Extensions
+    public static class Extensions
     {
-        public static string ReadEIOString(this BinaryReader reader)
+        public static void Write(this BinaryWriter writer, Bitfield8 bf)
         {
-            int lengthBytes = reader.ReadInt32();
-            return EIOSettings.TextEncoding.GetString(reader.ReadBytes(lengthBytes));
+            writer.Write(bf.GetByte());
         }
 
-        public static void WriteEIOString(this BinaryWriter writer, string data)
+        public static Bitfield8 ReadBitfield8(this BinaryReader reader)
         {
-            writer.Write(data.GetEIOByteCount());
-            writer.Write(data.GetEIOBytes());
-        }
-
-        public static int GetEIOByteCount(this string str)
-        {
-            return EIOSettings.TextEncoding.GetByteCount(str);
-        }
-
-        public static byte[] GetEIOBytes(this string str)
-        {
-            return EIOSettings.TextEncoding.GetBytes(str);
-        }
-
-        public static bool GetFlag(this byte field, int pos)
-        {
-            return ((field >> pos) & 0x1) == 1;
-        }
-
-        public static bool GetFlag(this short field, int pos)
-        {
-            return ((field >> pos) & 0x1) == 1;
-        }
-
-        public static bool GetFlag(this int field, int pos)
-        {
-            return ((field >> pos) & 0x1) == 1;
-        }
-
-        public static bool GetFlag(this long field, int pos)
-        {
-            return ((field >> pos) & 0x1) == 1;
+            return new Bitfield8(reader.ReadByte());
         }
     }
 }
